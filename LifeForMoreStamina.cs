833c975e1d0f7096ecd59a636e3b7acb18f5c8dc
Stif <stifskere@gmail.com>
2022-04-23 22:22:49 +0200

    Initial commit

---
+++ LifeForMoreStamina.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Assets._Scripts.Dissonance;
+using CommandSystem.Commands.RemoteAdmin.Doors;
+using Exiled.API.Features;
+using Exiled.Events;
+using Exiled.Events.EventArgs;
+using Interactables.Interobjects.DoorUtils;
+using Player = Exiled.Events.Handlers.Player;
+
+namespace LifeForMoreStamina
+{
+    public class LifeForMoreStamina : Plugin<PluginConfig>
+    {
+        public override string Author => "Memw#6969";
+        public override string Name => "LifeForMoreStamina";
+        public override Version Version => new Version(1, 0, 0);
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.ChangingMoveState += PlayerRunning;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.ChangingMoveState -= PlayerRunning;
+            base.OnDisabled();
+        }
+
+        private void PlayerRunning(ChangingMoveStateEventArgs ev)
+        {
+            if (ev.NewState != PlayerMovementState.Walking) return;
+            if (!(ev.Player.Stamina.RemainingStamina <= 0.015f)) return;
+            ev.Player.Stamina.RemainingStamina = 0.050f;
+            ev.Player.Health--;
+            ev.NewState = PlayerMovementState.Sprinting;
+        }
+    }
+}
---
+++ PluginConfig.cs
@@ -0,0 +1,9 @@
+﻿using Exiled.API.Interfaces;
+
+namespace LifeForMoreStamina
+{
+    public class PluginConfig : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+    }
+}
