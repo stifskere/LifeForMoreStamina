7b25c3fb9429dae9a809a883b0c1761338c0ac0d
Stif <stifskere@gmail.com>
2022-04-25 12:57:44 +0200

    Added lil descriptions to ploogin config

--- PluginConfig.cs
+++ PluginConfig.cs
@@ -1,10 +1,13 @@
 ﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
 
 namespace LifeForMoreStamina
 {
     public class PluginConfig : IConfig
     {
+        [Description("Defines if the plugin is enabled or it isn't")]
         public bool IsEnabled { get; set; } = true;
+        [Description("Defines how many life gets removed for every time stamina is depleted")]
         public int HpRemoved { get; set; } = 1;
     }
 }
