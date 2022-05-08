26270a82eb12535914eb23d9beada41cc51abb53
Stif <stifskere@gmail.com>
2022-05-09 00:26:54 +0200

    removed new lines

--- PluginConfig.cs
+++ PluginConfig.cs
@@ -9,7 +9,7 @@ namespace LifeForMoreStamina
         public bool IsEnabled { get; set; } = true;
         [Description("Defines how many life gets removed for every time stamina is depleted")]
         public int HpRemoved { get; set; } = 1;
-        [Description("Defines how many stamina gets added after the stamina gets to 0.025\n (There is no config for this because the counter bugs if you put a smaller value on there.)\n the values of the stamina are from 0 to 1 being 0.50 the half")]
+        [Description("Defines how many stamina gets added after the stamina gets to 0.025 (There is no config for this because the counter bugs if you put a smaller value on there.) the values of the stamina are from 0 to 1 being 0.50 the half")]
         public double StaminaAdded { get; set; } = 0.050;
     }
 }
